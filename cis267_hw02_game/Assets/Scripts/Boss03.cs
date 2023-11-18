d6f16c6d18acfcf20f5bb5dd0472cad4e0c25989
chaseothan <52476148+chaseothan@users.noreply.github.com>
2023-11-18 15:46:09 -0500

    boss03
    boss03

---
+++ cis267_hw02_game/Assets/Scripts/Boss03.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Boss03 : MonoBehaviour
+{
+    public float movementSpeed;
+    public float offset;
+    private bool moveLeft;
+    private float startPosx;
+    public float initialGameSpeed = 1f;
+    public float gameSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSpeed = initialGameSpeed;
+        startPosx = transform.position.x;
+        moveLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveBossRight();
+    }
+
+    public void moveBossRight()
+    {
+        transform.position += Vector3.right * gameSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
