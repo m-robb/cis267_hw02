b294ff010c062e14fe71c527ee8eb2afa5a64809
GabrielBall05 <gabrielb4556@gmail.com>
2023-11-22 17:12:07 -0500

    Temp fix for player health bar

--- cis267_hw02_game/Assets/Scripts/PlayerHealthBar.cs
+++ cis267_hw02_game/Assets/Scripts/PlayerHealthBar.cs
@@ -7,19 +7,8 @@ public class PlayerHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
 
-    public void setHealth(float health)
-    {
-        slider.value = health;
-    }
-
-    public void setMaxHealth(float health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
-    }
-
     public void updateHealthBar(float health, float maxHealth)
     {
-
+        slider.value = health / maxHealth;
     }
 }
